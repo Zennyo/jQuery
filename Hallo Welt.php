67ae900f6380b1de9bf92170f2dd3767e20ae374
Zenyo <s3nnyo@googlemail.com>
2013-03-05 10:03:27 +0100

    training
    training

---
+++ Hallo Welt.php
@@ -0,0 +1,28 @@
+<?php
+
+echo "<html>";
+
+/*
+for ($i=1; $i <=100 ; $i++) { 
+
+	echo $i."-";
+	# code...
+}
+*/
+
+?>
+
+
+<head>
+
+<title>jQuery testing</title>
+
+</head>
+
+<body>
+<h1>Hallo Welt !
+
+bla bla bla </h1>
+</body>
+
+</html>
